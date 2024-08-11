e5769154b04d007562664ca1e08d2532a1657747
Shivakant <kants6397@gmail.com>
2024-08-11 17:05:02 +0530

    debugging

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,16 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const cors = require('cors')
-const path=require("path")
-const cookieParser = require('cookie-parser')
-const multer=require('multer')
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
 
-
-
-const authRoute=require('./routes/auth')
-const userRoute=require('./routes/users')
-const postRoute=require('./routes/posts')
-const commentRoute=require('./routes/comments')
-const dotenv=require('dotenv')
-
-
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -24,49 +20,45 @@ app.use(express.json());
 
 //Third party middleware
 app.use(
-    cors({
-        origin: [process.env.FRONTEND_URL],
-        credentials: true
-    })
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
 );
 
-
-
-
 //Server status check
-app.get('/hello', (_req, res) => {
-    res.send('Hello world')
+app.get("/hello", (_req, res) => {
+  res.send("Hello world");
 });
 
-app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(cookieParser())
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/posts",postRoute)
-app.use("/api/comments",commentRoute)
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
 // Default catch all route - 404
-app.all('*', (_req, res) => {
-  res.status(404).send('OOPS!!! 404 Page Not Found');
+app.all("*", (_req, res) => {
+  res.status(404).send("OOPS!!! 404 Page Not Found");
 });
 
-
 //image upload
-const storage=multer.diskStorage({
-    destination:(req,file,fn)=>{
-        fn(null,"images")
-    },
-    filename:(req,file,fn)=>{
-        fn(null,req.body.img)
-        // fn(null,"image1.jpg")
-    }
-})
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+    // fn(null,"image1.jpg")
+  },
+});
 
-const upload=multer({storage:storage})
-app.post("/api/upload",upload.single("file"),(req,res)=>{
-    // console.log(req.body)
-    res.status(200).json("Image has been uploaded successfully!")
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  // console.log(req.body)
+  res.status(200).json("Image has been uploaded successfully!");
+});
 
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,48 @@
-const express=require('express')
-const app=express()
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-const cors=require('cors')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const multer=require('multer')
-const path=require("path")
-const cookieParser=require('cookie-parser')
-const authRoute=require('./routes/auth')
-const userRoute=require('./routes/users')
-const postRoute=require('./routes/posts')
-const commentRoute=require('./routes/comments')
-const { connect } = require('http2')
-const connectToDB = require('./config/connectDB')
+const multer = require("multer");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+const { connect } = require("http2");
+const connectToDB = require("./config/connectDB");
 // const app=require('./app')
 
-
 //middlewares
-dotenv.config()
-app.use(express.json())
-app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(cookieParser())
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/posts",postRoute)
-app.use("/api/comments",commentRoute)
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
 //image upload
-const storage=multer.diskStorage({
-    destination:(req,file,fn)=>{
-        fn(null,"images")
-    },
-    filename:(req,file,fn)=>{
-        fn(null,req.body.img)
-        // fn(null,"image1.jpg")
-    }
-})
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+    // fn(null,"image1.jpg")
+  },
+});
 
-const upload=multer({storage:storage})
-app.post("/api/upload",upload.single("file"),(req,res)=>{
-    // console.log(req.body)
-    res.status(200).json("Image has been uploaded successfully!")
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  // console.log(req.body)
+  res.status(200).json("Image has been uploaded successfully!");
+});
 
-
-app.listen(process.env.PORT,async()=>{
-    await connectToDB();
-    console.log(`App is running at http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, async () => {
+  await connectToDB();
+  console.log(`App is running at http://localhost:${process.env.PORT}`);
+});
